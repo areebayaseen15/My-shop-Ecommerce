231dc2d0750eded7be248a6c4316d3ba6da720f3
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-02-10 02:44:57 +0500

    Add files via upload

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,15 @@
+import CartContainer from '../component/CartContainar';
+import Container from '../component/Container';
+
+const CartPage = () => {
+ 
+  return (
+    <div>
+      <Container className="py-5">
+        <CartContainer />
+      </Container>
+    </div>
+  );
+};
+
+export default CartPage;
